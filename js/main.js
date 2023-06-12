884427828eef59909ad643758fcf32f27c6d2319
Franco Cunha Duarte <francunhaduarte@gmail.com>
2023-06-12 02:30:32 -0300

    Second delivery

--- js/main.js
+++ js/main.js
@@ -43,7 +43,7 @@ function comprobarCaptcha(e){
     }
     else{
         mostrar.innerHTML = "Captcha incorrecto, volver a ingresar";
-
+        
     }
 }
 
@@ -67,28 +67,33 @@ function hideForm(){
 let addName=document.querySelector('#name');
 let addPrice=document.querySelector('#price');
 let addCondition=document.querySelector('#condition');
+let addImage=document.querySelector('#file');
 let printError=document.querySelector('.error-condition');
-let table=[{
-    'nombre':addName.value,
-    'precio':addPrice.value,
-    'condicion':addCondition.value
+let imagesRandom=[
+    "../img/pc-gamer.webp","../img/shirts.jpg","../img/shoes.jpg","../img/shorts.png","../img/hats.png"
+]
+let producttable=[{
+    'imagen': imagesRandom[0],
+    'nombre': "Computadora Gamer Pro",
+    'precio': 2000,
+    'condicion': "nuevo"
 }];
+printTable();
 
-let addForm = document.querySelector('.addTable--button').addEventListener('click', function() {
+let buttonNewTable=document.querySelector('.addTable--button');
+buttonNewTable.addEventListener('click',addNewTable);
+function addNewTable(){
+    let randomNumber=Math.floor(Math.random()*imagesRandom.length);
+    let printError=document.querySelector('.error-condition');
     let names = addName.value;
     let price = addPrice.value;
     let condition = addCondition.value;
+    let image=addImage;
     let conditionToLowerCase = condition.toLowerCase();
+    let prube=false;
     if (conditionToLowerCase == "nuevo" || conditionToLowerCase == "usado") {
         printError.innerHTML = "";
-        table = {
-            'nombre': names,
-            'precio': price,
-            'condicion': conditionToLowerCase
-        };
-        let upgradeButton=document.querySelector('.addTable--button');
-        upgradeButton.innerHTML="Editar";
-        upgradeTable();
+        prube=true;
     } else if (conditionToLowerCase == "") {
         printError.innerHTML = "";
         printError.innerHTML = "Debe ingresar un valor para poder continuar.";
@@ -96,34 +101,55 @@ let addForm = document.querySelector('.addTable--button').addEventListener('clic
         printError.innerHTML = "";
         printError.innerHTML = "El valor ingresado está mal escrito o no es válido.";
     }
-});
-
-function upgradeTable(){
-    let nameTable=document.querySelector('.nameInTable');
-    nameTable.innerHTML=table.nombre;
-    let priceTable=document.querySelector('.priceInTable');
-    priceTable.innerHTML=table.precio;
-    let conditionTable=document.querySelector('.conditionInTable');
-    conditionTable.innerHTML=table.condicion;
+    if(prube){
+        newTable = {
+            'imagen':imagesRandom[randomNumber],
+            'nombre': names,
+            'precio': price,
+            'condicion': conditionToLowerCase,
+        };
+        producttable.push(newTable);
+        let upgradeButton=document.querySelector('.addTable--button');
+        upgradeButton.innerHTML="Nueva Tabla";
+        printTable();
+    }
 }
 
-let addRow=document.querySelector('.addRow').addEventListener('click',function(){
+
+function printTable(){
     let table=document.querySelector('.table');
-    let row=document.createElement('tr');
-    let inputName=document.querySelector('.name-input-addTable');
-    let inputAtribute=document.querySelector('.atribute-input-addTable');
-    
-    let leftColumn=document.createElement('td');
-    leftColumn.textContent="ASDS";
-    row.appendChild(leftColumn);
-    let rightColumn=document.createElement('td');
-    rightColumn.textContent="ASDS";
-    row.appendChild(rightColumn);
-    table.appendChild(row);
+    table.innerHTML="";
+    for(let atributo of producttable){
+        let nombre=atributo.nombre;
+        let precio=atributo.precio;
+        let condicion=atributo.condicion;
+        let image=atributo.imagen;
+        table.innerHTML +=
+            `<tr>
+                <td class="img-container"><img src=${image}></td>
+            </tr>
+            <tr>
+                <td>${nombre}</td>
+            </tr>
+            <tr>
+                <td>$ ${precio}</td>
+            </tr>
+            <tr>
+                <td class="space-table">${condicion}</td>
+            </tr>`;              
+    }
+}
+let tableEmpty=document.querySelector('.emptyTable--button').addEventListener('click',function(){
+    producttable=[];
+    printTable();
 });
 
-let delRow = document.querySelector('.delRow').addEventListener('click', function() {
-    let table = document.querySelector('.table');
-    let rows = table.getElementsByTagName('tr');
-        table.removeChild(rows[rows.length - 1]);
+let delLastItemButton=document.querySelector('.deletLastItemTable--button').addEventListener('click',function(){
+    producttable.pop();
+    printTable();
+});
+let addTablex3Button=document.querySelector('.addTablex3--button').addEventListener('click',function(){
+    for(let i=0;i<3;i++){
+        addNewTable();
+    }
 });
